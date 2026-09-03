3985deb72c7d896206149d1cefaaff0915e59a31
Rafaela Geiger <rafabelamk@gmail.com>
2026-09-02 21:59:51 -0300

    Update next.config.mjs

--- rafaela-site/next.config.mjs
+++ rafaela-site/next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com https://*.google-analytics.com https://tagmanager.google.com",
-              "img-src 'self' data: https://*.googletagmanager.com https://*.google-analytics.com",
-              "connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://stats.g.doubleclick.net",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googletagmanager.com https://*.google-analytics.com https://tagmanager.google.com https://connect.facebook.net",
+              "img-src 'self' data: https://*.googletagmanager.com https://*.google-analytics.com https://www.facebook.com",
+              "connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://stats.g.doubleclick.net https://www.facebook.com https://connect.facebook.net",
               "frame-src 'self' https://*.googletagmanager.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
